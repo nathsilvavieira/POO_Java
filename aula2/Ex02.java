7f38907b28a24dbe2a426b9abd8afe4607721eea
Nathalia Vieira <naahviera@gmail.com>
2021-08-11 22:04:29 -0300

    adicionando exercicio refatorado

--- aula2/Ex02.java
+++ aula2/Ex02.java
@@ -1,5 +1,9 @@
 package POO_DH.aula2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Ex02 {
     //Exercício 2
     //Escreva uma função que receba três números e retorne o máximo dos três números.
@@ -8,6 +12,10 @@ public class Ex02 {
     public static void main(String[] args) {
         Maximo maximo = new Maximo();
         System.out.println(maximo.maximoEntreTresNumeros(3,4,6));
+        RefatoradoMaximo maximoValor = new RefatoradoMaximo();
+
+        List<Integer> lista = Arrays.asList(1,20,3,4,50,6,7,8,9);
+        System.out.println(maximoValor.maximoValor(lista));
 
 
     }
--- aula2/RefatoradoMaximo.java
+++ aula2/RefatoradoMaximo.java
@@ -1,11 +1,13 @@
 package POO_DH.aula2;
 
+import java.util.List;
+
 public class RefatoradoMaximo {
-    public int maximoValor() {
-        int maximo = 0;
-        for (int i = 0; i < valor.length; i++) {
-            if (valor[i] > maximo) {
-                maximo = valor[i];
+    public Integer maximoValor(List<Integer> lista) {
+        Integer maximo = 0;
+        for (Integer i = 1; i < lista.size(); i++) {
+            if (lista.get(i) > maximo){
+                maximo = lista.get(i);
             }
         }
         return maximo;
